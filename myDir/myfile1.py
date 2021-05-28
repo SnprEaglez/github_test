dc3cbb8597d9fbb98bbb26169a8c858f0f3f6c6b
SnprEaglez <vabhi2007@icloud.com>
2021-05-27 18:22:40 -0700

    initial commit

---
+++ myDir/myfile1.py
@@ -0,0 +1,7 @@
+from myfile2 import myFunctionTwo
+def myFunctionOne ():
+   a = 1
+   print("Function one called and value of A is " + str(a))
+
+myFunctionOne()
+myFunctionTwo()
---
+++ myDir/myfile2.py
@@ -0,0 +1,3 @@
+def myFunctionTwo():
+   b = 2
+   print("Function two called and value of B is " + str(b))
